9c2823ccd65e1f61c3bfad3b9ce80cb30669c518
jeevnesh nandwana <jeevneshn@gmail.com>
2026-05-24 20:22:38 +0530

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+   private:
+    void mergeSortedArray(vector<int>& nums, int start, int mid, int end) {
+        int i = start, j = mid + 1;
+        vector<int> tempArr;
+        while (i <= mid && j <= end) {
+            if (nums[i] < nums[j]) {
+                tempArr.push_back(nums[i]);
+                i++;
+            } else {
+                tempArr.push_back(nums[j]);
+                j++;
+            }
+        }
+        while (i <= mid) {
+            tempArr.push_back(nums[i]);
+            i++;
+        }
+        while (j <= end) {
+            tempArr.push_back(nums[j]);
+            j++;
+        }
+        for (int i = 0; i < tempArr.size(); i++) {
+            nums[start + i] = tempArr[i];
+        }
+    }
+    void mergeSort(vector<int>& nums, int start, int end) {
+        if (start >= end) return;
+        int mid = start + (end - start) / 2;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid + 1, end);
+        mergeSortedArray(nums, start, mid, end);
+    }
+
+   public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
