040450c21356a2c4a4edb538dc4e40f8d7461796
jeevnesh nandwana <jeevneshn@gmail.com>
2026-05-25 18:51:47 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freqMap;
+        for (auto i : nums) {
+            freqMap[i]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        for (auto i : freqMap) {
+            minHeap.push({i.second, i.first});
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        vector<int> result;
+        while (!minHeap.empty()) {
+            result.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        return result;
+    }
+};
