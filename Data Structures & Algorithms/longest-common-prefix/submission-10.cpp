b651058640ec78bd308cd7734d303134955ca7ce
jeevnesh nandwana <jeevneshn@gmail.com>
2026-08-23 09:26:03 +0530

    Add: longest-common-prefix - submission-10

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-10.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(), strs.end());
+        int n = strs.size();
+        string ans = "";
+        for(int i = 0; i < strs[0].size(); i++){
+            if(strs[0][i] != strs[n-1][i]) break;
+            ans += strs[0][i];
+        }
+        return ans;
+    }
+};
